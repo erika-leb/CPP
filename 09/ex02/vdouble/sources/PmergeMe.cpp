e6d526f3cb9676673b53bca84939dc15a1cacdec
Erika Le Borgne <ele-borg@f2r7s23.clusters.42paris.fr>
2025-09-26 20:56:49 +0200

    version qui marchegit add .git add .

--- 09/ex02/vdouble/sources/PmergeMe.cpp
+++ 09/ex02/vdouble/sources/PmergeMe.cpp
@@ -7,7 +7,7 @@ PmergeMe::PmergeMe()
 PmergeMe::PmergeMe(char **av)
 {
     long n;
-    std::vector< std::pair<int, int> > lst;
+    std::vector< std::pair<int, std::pair<char, int> > > lst;
     // std::string d;
     int i = 1;
     while (av[i])
@@ -23,7 +23,7 @@ PmergeMe::PmergeMe(char **av)
         n = std::strtol(av[i], NULL, 10);
         if (n > INT_MAX)
             throw std::invalid_argument("invalid argument");
-        lst.push_back(std::make_pair(static_cast<int>(n), -1));
+        lst.push_back(std::make_pair(static_cast<int>(n), std::make_pair('x', -1)));
         i++;
     }
     _vect = lst;
@@ -71,68 +71,17 @@ int PmergeMe::jacobNb(int n)
         return (jacobNb(n - 1) + 2 * jacobNb(n - 2));
 }
 
-// int PmergeMe::getJacobNb(int n)
-// {
-//     if (n == 0)
-//         return jacobNb(3);
-//     else
-//         return    
-// }
-
-
-// void PmergeMe::insertPend(std::vector<int> &lst, std::vector<int> &pend)
-// {
-//     size_t i = 0;
-//     size_t k = 1;
-//     std::vector<std::pair<int, int> > pendBis;
-//     for(std::vector<int>::iterator it = pend.begin(); it != pend.end(); ++it)
-//     {
-//         if (k * _pairSize - 1 == i)
-//         {
-//             pendBis.push_back(std::make_pair(*it, k + 1));            
-//             k++;
-//         }
-//         else
-//             pendBis.push_back(std::make_pair(*it, -1));
-//         i++;
-//     }
-//     k = 1;
-//     i = 0;
-//     std::vector<std::pair<int, int> > mainBis;
-//     for(std::vector<int>::iterator it = lst.begin(); it != lst.end(); ++it)
-//     {
-//         // std::cout << "_pairSize = " << _pairSize << "; k * _pairSize - 1 =" << k * _pairSize - 1 << "; i = " << i << std::endl;
-//         if (k * _pairSize - 1 == i)
-//         {
-//             mainBis.push_back(std::make_pair(*it, k - 1));            
-//             k++;
-//         }
-//         else
-//             mainBis.push_back(std::make_pair(*it, -1));
-//         i++;
-//     }
-//     printPair(pendBis);
-//     printPair(mainBis);
-// }
-
-// void PmergeMe::insertPend(std::deque<int> &lst, std::deque<int> &pend)
-// {
-//     (void)lst;
-//     (void) pend;
-// }
-
-
 void PmergeMe::print(int n) const
 {
     if (n == 1)
     {
         std::cout << "Before:" << std::endl;
         std::cout << "vector: ";
-        for (std::vector< std::pair<int, int> >::const_iterator it = _vect.begin(); it != _vect.end(); ++it)
+        for (std::vector< std::pair<int, std::pair<char, int> > >::const_iterator it = _vect.begin(); it != _vect.end(); ++it)
             std::cout << it->first << " ";
         std::cout << std::endl;
         std::cout << "deque: ";
-        for (std::deque< std::pair<int, int> >::const_iterator it = _deq.begin(); it != _deq.end(); ++it)
+        for (std::deque< std::pair<int, std::pair<char, int> > >::const_iterator it = _deq.begin(); it != _deq.end(); ++it)
             std::cout << it->first << " ";
         std::cout << std::endl;
     }
@@ -140,11 +89,11 @@ void PmergeMe::print(int n) const
     {
         std::cout << "After:" << std::endl;
         std::cout << "vector: ";
-        for (std::vector<std::pair<int, int> >::const_iterator it = _vect.begin(); it != _vect.end(); ++it)
+        for (std::vector<std::pair<int, std::pair<char, int> > >::const_iterator it = _vect.begin(); it != _vect.end(); ++it)
             std::cout << it->first << " ";
         std::cout << std::endl;
         std::cout << "deque: ";
-        for (std::deque<std::pair<int, int> >::const_iterator it = _deq.begin(); it != _deq.end(); ++it)
+        for (std::deque<std::pair<int, std::pair<char, int> > >::const_iterator it = _deq.begin(); it != _deq.end(); ++it)
             std::cout << it->first << " ";
         std::cout << std::endl;
     }
@@ -156,7 +105,7 @@ void PmergeMe::print(int n) const
         //     std::cout << *it << " ";
         // std::cout << std::endl;
         std::cout << "deque: ";
-        for (std::deque< std::pair<int, int> >::const_iterator it = _deq.begin(); it != _deq.end(); ++it)
+        for (std::deque< std::pair<int, std::pair<char, int> > >::const_iterator it = _deq.begin(); it != _deq.end(); ++it)
             std::cout << it->first << " ";
         std::cout << std::endl;
     }
--- 09/ex02/vdouble/sources/PmergeMe.hpp
+++ 09/ex02/vdouble/sources/PmergeMe.hpp
@@ -32,7 +32,7 @@ class PmergeMe {
             std::advance(itbegin, begin);
             std::advance(itend, end + 1);
 
-            std::vector< std::pair<int, int> > tmp(itbegin, itend);
+            std::vector< std::pair<int, std::pair<char, int> > > tmp(itbegin, itend);
             lst.erase(itbegin, itend); //ici aussi
             typename T::iterator itpos = lst.begin();
             if (pos >= lst.size())
@@ -70,12 +70,13 @@ class PmergeMe {
 
 
         template <typename T>
-        T createPend(T &lst)
+        T createPend(T &lst, T&remain)
         {
             T pend;
             int k = 3;
             size_t i = 0;
             size_t j = 1;
+			// int l;
 
             std::cerr << "avant modif" << std::endl;
             printCont(pend);
@@ -93,15 +94,53 @@ class PmergeMe {
 
                 k++;
             }
+			k = 1;
+			while (k * _pairSize - 1 < lst.size())
+				k++;
+			k--;
+			if (k * _pairSize < lst.size())
+			{
+				remain.insert(remain.begin(), lst.begin() + k * _pairSize, lst.end());
+				lst.erase(lst.begin() + k * _pairSize, lst.end());
+			}
+
+			// if (lst.begin() + l != lst.end())
+			// {
+			// 	std::cout << "l = " << l << std::endl;
+			// 	std::cout << "I " << std::endl;
+
+			// 	std::cout << "don't" << std::endl;
+			// }
+			// std::cout << "(k * _pairSize) - 1 = " << (k * _pairSize) - 1 << std::endl;
+			// std::cout << "((k - 1) * _pairSize) - 1 = " << ((k - 1) * _pairSize) - 1 << std::endl;
+			// std::cout << "man" << std::endl;
+			// l = (k - 1) * _pairSize - 1;
+			// if (lst.begin() + l != lst.end())
+			// {
+			// 	std::cout << "l = " << l << std::endl;
+			// 	remain.insert(remain.begin(), lst.begin() + l, lst.end());
+			// 	std::cout << "I " << std::endl;
+			// 	lst.erase(lst.begin() + l, lst.end());
+			// 	std::cout << "don't" << std::endl;
+			// }
+			// while (l < lst.size())
+			// {
+			// 	remain.insert(remain.end(), lst.begin() + l, )
+			// 	l++;
+			// }
             for(typename T::iterator it = pend.begin(); it != pend.end(); ++it)
             {
                 if (j * _pairSize - 1 == i)
                 {
-                    it->second = j + 1;
+					it->second.first = 'b';
+                    it->second.second = j + 1;
                     j++;
                 }
                 else
-                    it->second = -1;
+				{
+					it->second.first = 'b';
+                    it->second.second = -1;
+				}
                 i++;
             }
             j = 1;
@@ -110,17 +149,23 @@ class PmergeMe {
             {
                 if (j * _pairSize - 1 == i)
                 {
-                    it->second = j - 1;
+					it->second.first = 'a';
+                    it->second.second= j - 1;
                     j++;
                 }
                 else
-                    it->second = -1;
+				{
+					it->second.first = 'b';
+                    it->second.second = -1;
+
+				}
                 i++;
             }
 
             std::cerr << "apres modif create pend (pend et lst)" << std::endl;
             printCont(pend);
             printCont(lst);
+			printCont(remain);
             std::cerr << "fin" << std::endl;
             return (pend);
         }
@@ -130,7 +175,7 @@ class PmergeMe {
         {
             int res;
             for (typename T::iterator it = pend.begin(); it != pend.end(); ++it) //ne sert a rien en soit
-                res = it->second;
+                res = it->second.second;
             return res; // a verifier
         }
 
@@ -142,26 +187,26 @@ class PmergeMe {
 
             // std::cerr << "all night, print pend = ";
             // printPair(pend);
-            while (it->second != value)
+            while (it->second.second != value)
                 ++it;
-            // std::cout << "haha  it first = " << it->first << "; second = " << it->second << std::endl;
+            // std::cout << "haha  it first = " << it->first << "; second.second = " << it->second.second << std::endl;
             if (it == pend.end()) //inutile normalement
                 return tmp;
             while (it != pend.begin())
             {
                 // std::cerr << "dishes" << std::endl;
-                // std::cout << "it first = " << it->first << "; second = " << it->second << std::endl;
-                if (it->second != (-1))
+                // std::cout << "it first = " << it->first << "; second.second = " << it->second.second << std::endl;
+                if (it->second.second != (-1))
                 {
-                    // std::cout << "r = " << _pairSize << "; first = "<< it->first <<"; it second = "<< it->second << std::endl;
+                    // std::cout << "r = " << _pairSize << "; first = "<< it->first <<"; it second.second = "<< it->second.second << std::endl;
                     tmp.insert(tmp.end(), it - _pairSize + 1, it + 1);
                 }
                 it--;
-                // std::cout << "first = "<< it->first <<"; it second = "<< it->second << std::endl;
+                // std::cout << "first = "<< it->first <<"; it second.second = "<< it->second.second << std::endl;
             }
             if (it == pend.begin() && _pairSize == 1)
                 tmp.insert(tmp.end(), it - _pairSize + 1, it + 1);
-            // while (it != pend.end() && it->second <= value)
+            // while (it != pend.end() && it->second.second <= value)
             // {
             //     tmp.insert(tmp.begin(), *it);
             //     // tmp.push_back(*it);
@@ -174,6 +219,12 @@ class PmergeMe {
             return (tmp);
         }
 
+		template <typename T>
+		void changeValue(typename T::iterator tmpitbegin, typename T::iterator tmpitend)
+		{
+			for (typename T::iterator it = tmpitbegin; it != tmpitend; it++)
+				it->second.second = -1;
+		}
 
         template <typename T>
         void insertBloc(T &lst, T &tmp) // il faut comparer tmp et lst, a la fin, les elements de tmp doivent avoir ete insere dans lst
@@ -181,20 +232,18 @@ class PmergeMe {
             std::cerr << "AVANT = " << std::endl;
             printPair(tmp);
             printPair(lst);
-			std::cerr << "r = " << _pairSize << std::endl;
-            // size_t k = 1;
-            // size_t i = 0;
+			// std::cerr << "r = " << _pairSize << std::endl;
             for (typename T::iterator tmpIt = tmp.begin(); tmpIt != tmp.end(); ++tmpIt)
             {
-                if (tmpIt->second != -1)
+                if (tmpIt->second.second != -1)
                 {
-					std::cout << "paul" << std::endl;
+					// std::cout << "paul" << std::endl;
                     // for (typename T::iterator ite = lst.begin(); ite != lst.end(); ++ite)
-                    for (typename T::iterator lstIt = lst.begin(); lstIt != lst.end() && lstIt->second != tmpIt->second + 2; ++lstIt)
+                    for (typename T::iterator lstIt = lst.begin(); lstIt != lst.end() && !(lstIt->second.first == 'a' && lstIt->second.second == tmpIt->second.second + 2); ++lstIt)
                     {
                         // k = 1;
-                        std::cout << "kim et lstIT second ="  << lstIt->second<< std::endl;
-                        if (lstIt->second != -1)
+                        std::cout << "kim et lstIT second.second ="  << lstIt->second.second<< std::endl;
+                        if (lstIt->second.second != -1)
                         {
                             std::cout <<"yas et tmpIT =" << tmpIt->first << "; lstIT first = " << lstIt->first<< std::endl;
                             if ((lstIt + 1) == lst.end() && tmpIt->first > lstIt->first)//si on est au dernier element de lst
@@ -211,10 +260,10 @@ class PmergeMe {
                                 // std::advance(tmpitend, k * _pairSize);
                                 // std::advance(lstitpos, i * _pairSize);
 								std::cout << "adri" << std::endl;
-                                // std::cout << "el ; valeur de lst ou on va se positionner = " << lstitpos->first << std::endl;
+                                std::cout << "el ; valeur de lst ou on va se positionner = " << lstitpos->first << std::endl;
                                 std::cout <<"el ;valeur de debut de tmp a indroduire = " << tmpitbegin->first << std::endl;
-                                // std::cout << "; tmpend = " << tmpitend->first << std::endl;
-                                lst.insert(lstitpos, tmpitbegin, tmpitend);
+                                // changeValue<T>(tmpitbegin, tmpitend);
+								lst.insert(lstitpos, tmpitbegin, tmpitend);
                                 // lst.insert(lstIt - _pairSize, tmpIt - _pairSize, tmpIt + 1);
                                 // std::cerr << "eNTRE TEMPS= " << std::endl;
                                 // printPair(tmp);
@@ -240,24 +289,16 @@ class PmergeMe {
                                 // std::advance(lstitpos, i * _pairSize);
                                 std::cout << "valeur de lst ou on va se positionner = " << lstitpos->first << std::endl;
                                 std::cout <<"valeur de debut de tmp a indroduire = " << tmpitbegin->first << std::endl;
-                                // std::cout << "; tmpend = " << tmpitend->first << std::endl;
-                                lst.insert(lstitpos, tmpitbegin, tmpitend);
+                                // changeValue<T>(tmpitbegin, tmpitend);
+								lst.insert(lstitpos, tmpitbegin, tmpitend);
                                 // lst.insert(lstIt - _pairSize, tmpIt - _pairSize, tmpIt + 1);
                                 // std::cerr << "eNTRE TEMPS= " << std::endl;
                                 // printPair(tmp);
                                 // printPair(lst);
                                 break ;
-                                // i++;
                             }
-                            // k++;
-                            // std::cout << "on est sorti de cette boucle" << std::endl;
                         }
-                        // std::cout << "on est sorti de la boucle" << std::endl;
-                        // break ;
-                        // i++;
                     }
-                    // on parcours la lst de 0 a ite->second pour comparer
-                    // on insere le bloc de (k - 1) * _pair a k * _pair - 1 inclus
                 }
             }
             std::cerr << "APRES (2eme liste doit contenir la première)= " << std::endl;
@@ -265,7 +306,36 @@ class PmergeMe {
             printPair(lst);
         }
 
-        template <typename T>
+        // template <typename T>
+        // typename T::iterator deleteBloc(int value, T &tmp, T&pend)
+        // {
+        // //     if (!pend.empty())
+        // //     {
+        // //         std::cout << "delete pend bef";
+        // //         printCont(pend);
+        // //     }
+        //     tmp.clear();
+
+		// 	printPair(pend);
+        //     typename T::iterator it = pend.begin();
+        //     while (it != pend.end() && it->second.second <= value)
+        //     {
+		// 		std::cout << "ICI it->first = " << it->first << "; it->second.second = " << it->second.second << std::endl;
+        //         if (it->second.second <= value)
+        //             it = pend.erase(it);
+        //         else
+        //             it = it + 1;
+		// 			// it = pend.erase(it, pend.end());
+        //     }
+        //     // if (!pend.empty())
+        //     // {
+        //     //     std::cout << "delete pend aft";
+        //     //     printCont(pend);
+        //     // }
+        //     return (it);
+        // }
+
+		template <typename T>
         typename T::iterator deleteBloc(int value, T &tmp, T&pend)
         {
         //     if (!pend.empty())
@@ -273,13 +343,41 @@ class PmergeMe {
         //         std::cout << "delete pend bef";
         //         printCont(pend);
         //     }
-            tmp.clear();
+			// typename T::iterator it;
+            // tmp.clear();
+			// // int k = 1;
+			// printPair(pend);
+			// for (it = pend.begin() + (_pairSize - 1); it != pend.end(); it += _pairSize - 1)
+            // // while (it != pend.end() && it->second.second <= value)
+            // {
+			// 	std::cout << "ICI it->first = " << it->first << "; it->second.second = " << it->second.second << std::endl;
+            //     if (it->second.second <= value)
+            //         it = pend.erase(it - (_pairSize - 1), it + 1);
+            //     // else
+            //     //     it = it + 1;
+			// 		// it = pend.erase(it, pend.end());
+            // }
+            // // if (!pend.empty())
+            // // {
+            // //     std::cout << "delete pend aft";
+            // //     printCont(pend);
+            // // }
 
+            tmp.clear();
+			// std::cerr << "AV Delte Bloc; value = " << value << "; bloc pend = " << std::endl;
+            // printCont(pend);
+			// printPair(pend);
             typename T::iterator it = pend.begin();
-            while (it != pend.end() && it->second <= value)
+			int f = 0;
+            while (it != pend.end() && it->second.second <= value && f == 0)
             {
-                if (it->second <= value)
+                if (it->second.second <= value)
+				{
+					if (it->second.second == value)
+						f = 1;
                     it = pend.erase(it);
+
+				}
                 else
                     ++it;
             }
@@ -288,8 +386,14 @@ class PmergeMe {
             //     std::cout << "delete pend aft";
             //     printCont(pend);
             // }
+            // std::cerr << "AP r = " << _pairSize << "pend = ";
+            // printCont(pend);
+
+
             return (it);
         }
+
+
 
         template <typename T>
         void insertPend(T &lst, T &pend)
@@ -309,13 +413,13 @@ class PmergeMe {
             // printCont(pend);
             // for (typename T::iterator it = pend.begin(); it != pend.end(); ++it)
             // {
-            //     if (it->second == last_v || it->second == jacob)
+            //     if (it->second.second == last_v || it->second.second == jacob)
             //     {
-            //         tmp = createBloc(it->second, pend);
+            //         tmp = createBloc(it->second.second, pend);
             //         insertBloc(lst, tmp);
             //         std::cout << "like";
             //         printCont(pend);
-            //         it = deleteBloc(it->second, tmp, pend);
+            //         it = deleteBloc(it->second.second, tmp, pend);
             //         break;
             //     }
             // } // ci dessous a decommenter
@@ -325,9 +429,9 @@ class PmergeMe {
                 typename T::iterator it = pend.begin();
                 while (it != pend.end()) //ne sert a rien en soit
                 {
-                    if (it->second == jacob) //mise à jour du nombre de jacobsthal
+                    if (it->second.second == jacob) //mise à jour du nombre de jacobsthal
                     {
-                        tmp = createBloc(it->second, pend); // autre liste inversee
+                        tmp = createBloc(it->second.second, pend); // autre liste inversee
                         std::cout << "r = " << _pairSize << " y BEFORE = ";
                         printCont(tmp);
                         printCont(pend);
@@ -337,7 +441,7 @@ class PmergeMe {
                         printCont(tmp);
                         printCont(pend);
                         printCont(lst);
-                        it = deleteBloc(it->second, tmp, pend); //delete bloc et temp
+                        it = deleteBloc(it->second.second, tmp, pend); //delete bloc et temp
                         // k--;
                         v++;
                         jacob = jacobNb(3 + v);
@@ -348,13 +452,25 @@ class PmergeMe {
                         // std::cout << "ici" << std::endl;
                         break;
                     }
-                    else if (it->second == last_v)
+                    else if (it->second.second == last_v)
                     {
-                        // std::cout << "puis la" << std::endl;
+                        std::cout << "puis la" << std::endl;
+						std::cout << "r = " << _pairSize << " y BEFORE = ";
+                        printCont(tmp);
+                        printCont(pend);
+                        printCont(lst);
                         tmp = createBloc(last_v, pend); // autre liste inversee
                         insertBloc(lst, tmp); //insert dans liste
+						std::cout << "BETWIN (lst doit être complète ici) = ";
+                        printCont(tmp);
+                        printCont(pend);
+                        printCont(lst);
                         it = deleteBloc(last_v, tmp, pend); //delete bloc et temp
-                        break;
+                        std::cout << "AFTER (pend + lst = total) = ";
+                        printCont(tmp);
+                        printCont(pend);
+                        printCont(lst);
+						break;
                     }
                     else
                         ++it;
@@ -378,6 +494,7 @@ class PmergeMe {
         void mergeInsert(T &lst)
         {
             int f = 0;
+
             // std::cout << "j0  = " << jacobNb(0) << std::endl;
             // std::cout << "dishes "
             // _pairSize /= 2; //pour le premier on est a 8
@@ -385,7 +502,7 @@ class PmergeMe {
             while (_pairSize >= 1)
             {
                 // std::cout << "r = " << _pairSize << " size = " <<lst.size() << std::endl;
-                if (f == 0 && _pairSize * 3 > lst.size()) // revoir caaa
+                if (f == 0 && _pairSize * 3 > lst.size()) // revoir caaa utile ??
                 {
 					std::cout << "dishes " << std::endl;
                     // std::cout << "r = " << _pairSize << " et on a passe" << std::endl;
@@ -394,11 +511,18 @@ class PmergeMe {
                     continue;
                 }
                 // std::cout << "verif r = " << _pairSize << std::endl;
-                T pend = createPend(lst);
+				T remain;
+                T pend = createPend(lst, remain); // on recupere le reste dans un tableau
+
                 printCont(pend);
                 printCont(lst);
                 insertPend(lst, pend);
+				lst.insert(lst.end(), remain.begin(), remain.end());
+				remain.clear();
+				// on remet le reste dans lst et on supprime le reste
                 _pairSize /= 2; //pour le premier on est a 8
+				std::cerr << "TOUTE FIN DE TOUR = " << std::endl;
+            	printPair(lst);
                 // f = 1;
             }
             _pairSize = 1;
@@ -434,7 +558,7 @@ class PmergeMe {
             std::cout << "container de pair:";
             for (typename T::const_iterator it = lst.begin(); it != lst.end(); ++it)
             {
-                std::cout << "(" << it->first << "," << it->second << ") ";
+                 std::cout << "(" << it->first << ",(" << it->second.first << "," << it->second.second << ")) ";
             }
             std::cout << std::endl;
         }
@@ -448,8 +572,8 @@ class PmergeMe {
         void print(int n) const;
 
     private:
-        std::vector< std::pair<int, int> > _vect;
-        std::deque < std::pair<int, int> > _deq;
+        std::vector< std::pair<int, std::pair<char, int> > > _vect;
+        std::deque < std::pair<int, std::pair<char, int> > > _deq;
         size_t _pairSize;
 
         PmergeMe();
